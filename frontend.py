c8de601dbda46074f73def3918c7ca7696e26879
Taylor Childers <jchilders@alcfwl055.alcf.anl.gov>
2018-08-21 15:29:02 -0500

    removing anchor IOU calc

--- frontend.py
+++ frontend.py
@@ -93,7 +93,7 @@ class YOLO(object):
         mask_shape = tf.shape(y_true)[:4]
         
         cell_x = tf.to_float(tf.reshape(tf.tile(tf.range(self.grid_w), [self.grid_h]), (1, self.grid_h, self.grid_w, 1, 1)))
-        tf.Print(cell_x.shape,cell_x)
+        #tf.Print(cell_x.shape,cell_x)
         cell_y = tf.to_float(tf.reshape(tf.tile(tf.range(self.grid_h), [self.grid_w]), (1, self.grid_w, self.grid_h, 1, 1)))
         cell_y = tf.transpose(cell_y, (0,2,1,3,4))
 
@@ -133,6 +133,7 @@ class YOLO(object):
         ### adjust w and h
         true_box_wh = y_true[..., 2:4] # number of cells accross, horizontally and vertically
         
+        '''
         ### adjust confidence
         true_wh_half = true_box_wh / 2.
         true_mins    = true_box_xy - true_wh_half
@@ -152,8 +153,8 @@ class YOLO(object):
 
         union_areas = pred_areas + true_areas - intersect_areas
         iou_scores  = tf.truediv(intersect_areas, union_areas)
-        
-        true_box_conf = iou_scores * y_true[..., 4]
+        '''
+        true_box_conf = y_true[..., 4]
         
         ### adjust class probabilities
         true_box_class = tf.argmax(y_true[..., 5:], -1)
@@ -164,6 +165,7 @@ class YOLO(object):
         ### coordinate mask: simply the position of the ground truth boxes (the predictors)
         coord_mask = tf.expand_dims(y_true[..., 4], axis=-1) * self.coord_scale
         
+        '''
         ### confidence mask: penelize predictors + penalize boxes with low IOU
         # penalize the confidence of the boxes, which have IOU with some ground truth box < 0.6
         true_xy = self.true_boxes[..., 0:2]
@@ -190,29 +192,31 @@ class YOLO(object):
 
         union_areas = pred_areas + true_areas - intersect_areas
         iou_scores  = tf.truediv(intersect_areas, union_areas)
-
-        best_ious = tf.reduce_max(iou_scores, axis=4)
-        conf_mask = conf_mask + tf.to_float(best_ious < 0.6) * (1 - y_true[..., 4]) * self.no_object_scale
         
+        best_ious = tf.reduce_max(iou_scores, axis=4)
+
+        conf_mask = conf_mask + tf.to_float(best_ious < 0.6) * (1 - y_true[..., 4]) * self.no_object_scale
+        '''
+
         # penalize the confidence of the boxes, which are reponsible for corresponding ground truth box
         conf_mask = conf_mask + y_true[..., 4] * self.object_scale
         
         ### class mask: simply the position of the ground truth boxes (the predictors)
-        class_mask = y_true[..., 4] * tf.gather(self.class_wt, true_box_class) * self.class_scale       
+        class_mask = y_true[..., 4] * tf.gather(self.class_wt, true_box_class) * self.class_scale
         
         """
         Warm-up training
         """
-        no_boxes_mask = tf.to_float(coord_mask < self.coord_scale/2.)
+        no_boxes_mask = tf.to_float(coord_mask < self.coord_scale / 2.)
         seen = tf.assign_add(seen, 1.)
         
-        true_box_xy, true_box_wh, coord_mask = tf.cond(tf.less(seen, self.warmup_batches+1), 
-                              lambda: [true_box_xy + (0.5 + cell_grid) * no_boxes_mask, 
-                                       true_box_wh + tf.ones_like(true_box_wh) * \
-                                       np.reshape(self.anchors, [1,1,1,self.nb_box,2]) * \
-                                       no_boxes_mask, 
+        true_box_xy, true_box_wh, coord_mask = tf.cond(tf.less(seen, self.warmup_batches + 1),
+                              lambda: [true_box_xy + (0.5 + cell_grid) * no_boxes_mask,
+                                       true_box_wh + tf.ones_like(true_box_wh) *
+                                       np.reshape(self.anchors, [1,1,1,self.nb_box,2]) *
+                                       no_boxes_mask,
                                        tf.ones_like(coord_mask)],
-                              lambda: [true_box_xy, 
+                              lambda: [true_box_xy,
                                        true_box_wh,
                                        coord_mask])
         
@@ -223,13 +227,13 @@ class YOLO(object):
         nb_conf_box  = tf.reduce_sum(tf.to_float(conf_mask  > 0.0))
         nb_class_box = tf.reduce_sum(tf.to_float(class_mask > 0.0))
         
-        loss_xy    = tf.reduce_sum(tf.square(true_box_xy-pred_box_xy)     * coord_mask) / (nb_coord_box + 1e-6) / 2.
-        loss_wh    = tf.reduce_sum(tf.square(true_box_wh-pred_box_wh)     * coord_mask) / (nb_coord_box + 1e-6) / 2.
-        loss_conf  = tf.reduce_sum(tf.square(true_box_conf-pred_box_conf) * conf_mask)  / (nb_conf_box  + 1e-6) / 2.
+        loss_xy    = tf.reduce_sum(tf.square(true_box_xy - pred_box_xy) * coord_mask) / (nb_coord_box + 1e-6) / 2.
+        loss_wh    = tf.reduce_sum(tf.square(true_box_wh - pred_box_wh) * coord_mask) / (nb_coord_box + 1e-6) / 2.
+        loss_conf  = tf.reduce_sum(tf.square(true_box_conf - pred_box_conf) * conf_mask)  / (nb_conf_box  + 1e-6) / 2.
         loss_class = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=true_box_class, logits=pred_box_class)
         loss_class = tf.reduce_sum(loss_class * class_mask) / (nb_class_box + 1e-6)
         
-        loss = tf.cond(tf.less(seen, self.warmup_batches+1), 
+        loss = tf.cond(tf.less(seen, self.warmup_batches + 1),
                       lambda: loss_xy + loss_wh + loss_conf + loss_class + 10,
                       lambda: loss_xy + loss_wh + loss_conf + loss_class)
         
@@ -237,8 +241,8 @@ class YOLO(object):
             nb_true_box = tf.reduce_sum(y_true[..., 4])
             nb_pred_box = tf.reduce_sum(tf.to_float(true_box_conf > 0.5) * tf.to_float(pred_box_conf > 0.3))
             
-            current_recall = nb_pred_box/(nb_true_box + 1e-6)
-            total_recall = tf.assign_add(total_recall, current_recall) 
+            current_recall = nb_pred_box / (nb_true_box + 1e-6)
+            total_recall = tf.assign_add(total_recall, current_recall)
 
             loss = tf.Print(loss, [loss_xy], message='Loss XY \t', summarize=1000)
             loss = tf.Print(loss, [loss_wh], message='Loss WH \t', summarize=1000)
@@ -246,7 +250,7 @@ class YOLO(object):
             loss = tf.Print(loss, [loss_class], message='Loss Class \t', summarize=1000)
             loss = tf.Print(loss, [loss], message='Total Loss \t', summarize=1000)
             loss = tf.Print(loss, [current_recall], message='Current Recall \t', summarize=1000)
-            loss = tf.Print(loss, [total_recall/seen], message='Average Recall \t', summarize=1000)
+            loss = tf.Print(loss, [total_recall / seen], message='Average Recall \t', summarize=1000)
         
         return loss
 
